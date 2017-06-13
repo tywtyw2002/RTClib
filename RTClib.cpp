dca031b98a9734f78b2f46b93cde10a8f097870c
Tianyi Wu <tywtyw2002@gmail.com>
2017-06-12 20:31:30 -0600

    Brzo I2C

--- RTClib.cpp
+++ RTClib.cpp
@@ -1,48 +1,48 @@
 // Code by JeeLabs http://news.jeelabs.org/code/
 // Released to the public domain! Enjoy!
 
-#include <Wire.h>
+#include "brzo_i2c.h"
 #include "RTClib.h"
-#ifdef __AVR__
- #include <avr/pgmspace.h>
-#elif defined(ESP8266)
- #include <pgmspace.h>
-#elif defined(ARDUINO_ARCH_SAMD)
-// nothing special needed
-#elif defined(ARDUINO_SAM_DUE)
- #define PROGMEM
- #define pgm_read_byte(addr) (*(const unsigned char *)(addr))
- #define Wire Wire1
-#endif
+#include <pgmspace.h>
 
-
-
-#if (ARDUINO >= 100)
- #include <Arduino.h> // capital A so it is error prone on case-sensitive filesystems
- // Macro to deal with the difference in I2C write functions from old and new Arduino versions.
- #define _I2C_WRITE write
- #define _I2C_READ  read
-#else
- #include <WProgram.h>
- #define _I2C_WRITE send
- #define _I2C_READ  receive
-#endif
-
+uint16_t I2C_SCL_FREQUENCY;
+uint8_t buffer[8];
 
 static uint8_t read_i2c_register(uint8_t addr, uint8_t reg) {
-  Wire.beginTransmission(addr);
-  Wire._I2C_WRITE((byte)reg);
-  Wire.endTransmission();
+  //Wire.beginTransmission(addr);
+  //Wire._I2C_WRITE((byte)reg);
+  //Wire.endTransmission();
+  uint8_t bcode = 0;
+  uint8_t data;
 
-  Wire.requestFrom(addr, (byte)1);
-  return Wire._I2C_READ();
+  brzo_i2c_start_transaction(addr, I2C_SCL_FREQUENCY);
+  buffer[0] = reg;
+  brzo_i2c_write(buffer, 1, false);
+  bcode = brzo_i2c_end_transaction();
+
+  delay(50);
+
+  if (bcode == 0) {
+        brzo_i2c_start_transaction(addr, I2C_SCL_FREQUENCY);
+        brzo_i2c_read(data, 1, false);
+        bcode = brzo_i2c_end_transaction();
+  }
+
+  //Wire.requestFrom(addr, (byte)1);
+  //return Wire._I2C_READ();
+  return data;
 }
 
 static void write_i2c_register(uint8_t addr, uint8_t reg, uint8_t val) {
-  Wire.beginTransmission(addr);
-  Wire._I2C_WRITE((byte)reg);
-  Wire._I2C_WRITE((byte)val);
-  Wire.endTransmission();
+  brzo_i2c_start_transaction(addr, I2C_SCL_FREQUENCY);
+  buffer[0] = reg;
+  buffer[1] = val;
+  brzo_i2c_write(buffer, 2, false);
+  brzo_i2c_end_transaction();
+  //Wire.beginTransmission(addr);
+  //Wire._I2C_WRITE((byte)reg);
+  //Wire._I2C_WRITE((byte)val);
+  //Wire.endTransmission();
 }
 
 
@@ -131,7 +131,7 @@ static uint8_t conv2d(const char* p) {
 DateTime::DateTime (const char* date, const char* time) {
     // sample input: date = "Dec 26 2009", time = "12:34:56"
     yOff = conv2d(date + 9);
-    // Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec 
+    // Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
     switch (date[0]) {
         case 'J': m = (date[1] == 'a') ? 1 : ((date[2] == 'n') ? 6 : 7); break;
         case 'F': m = 2; break;
@@ -174,7 +174,7 @@ DateTime::DateTime (const __FlashStringHelper* date, const __FlashStringHelper* 
     ss = conv2d(buff + 6);
 }
 
-uint8_t DateTime::dayOfTheWeek() const {    
+uint8_t DateTime::dayOfTheWeek() const {
     uint16_t day = date2days(yOff, m, d);
     return (day + 6) % 7; // Jan 1, 2000 is a Saturday, i.e. returns 6
 }
@@ -231,208 +231,12 @@ TimeSpan TimeSpan::operator-(const TimeSpan& right) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// RTC_DS1307 implementation
-
+// RTC_DS3231 implementation
 static uint8_t bcd2bin (uint8_t val) { return val - 6 * (val >> 4); }
 static uint8_t bin2bcd (uint8_t val) { return val + 6 * (val / 10); }
 
-boolean RTC_DS1307::begin(void) {
-  Wire.begin();
-  return true;
-}
-
-uint8_t RTC_DS1307::isrunning(void) {
-  Wire.beginTransmission(DS1307_ADDRESS);
-  Wire._I2C_WRITE((byte)0);
-  Wire.endTransmission();
-
-  Wire.requestFrom(DS1307_ADDRESS, 1);
-  uint8_t ss = Wire._I2C_READ();
-  return !(ss>>7);
-}
-
-void RTC_DS1307::adjust(const DateTime& dt) {
-  Wire.beginTransmission(DS1307_ADDRESS);
-  Wire._I2C_WRITE((byte)0); // start at location 0
-  Wire._I2C_WRITE(bin2bcd(dt.second()));
-  Wire._I2C_WRITE(bin2bcd(dt.minute()));
-  Wire._I2C_WRITE(bin2bcd(dt.hour()));
-  Wire._I2C_WRITE(bin2bcd(0));
-  Wire._I2C_WRITE(bin2bcd(dt.day()));
-  Wire._I2C_WRITE(bin2bcd(dt.month()));
-  Wire._I2C_WRITE(bin2bcd(dt.year() - 2000));
-  Wire.endTransmission();
-}
-
-DateTime RTC_DS1307::now() {
-  Wire.beginTransmission(DS1307_ADDRESS);
-  Wire._I2C_WRITE((byte)0);	
-  Wire.endTransmission();
-
-  Wire.requestFrom(DS1307_ADDRESS, 7);
-  uint8_t ss = bcd2bin(Wire._I2C_READ() & 0x7F);
-  uint8_t mm = bcd2bin(Wire._I2C_READ());
-  uint8_t hh = bcd2bin(Wire._I2C_READ());
-  Wire._I2C_READ();
-  uint8_t d = bcd2bin(Wire._I2C_READ());
-  uint8_t m = bcd2bin(Wire._I2C_READ());
-  uint16_t y = bcd2bin(Wire._I2C_READ()) + 2000;
-  
-  return DateTime (y, m, d, hh, mm, ss);
-}
-
-Ds1307SqwPinMode RTC_DS1307::readSqwPinMode() {
-  int mode;
-
-  Wire.beginTransmission(DS1307_ADDRESS);
-  Wire._I2C_WRITE(DS1307_CONTROL);
-  Wire.endTransmission();
-  
-  Wire.requestFrom((uint8_t)DS1307_ADDRESS, (uint8_t)1);
-  mode = Wire._I2C_READ();
-
-  mode &= 0x93;
-  return static_cast<Ds1307SqwPinMode>(mode);
-}
-
-void RTC_DS1307::writeSqwPinMode(Ds1307SqwPinMode mode) {
-  Wire.beginTransmission(DS1307_ADDRESS);
-  Wire._I2C_WRITE(DS1307_CONTROL);
-  Wire._I2C_WRITE(mode);
-  Wire.endTransmission();
-}
-
-void RTC_DS1307::readnvram(uint8_t* buf, uint8_t size, uint8_t address) {
-  int addrByte = DS1307_NVRAM + address;
-  Wire.beginTransmission(DS1307_ADDRESS);
-  Wire._I2C_WRITE(addrByte);
-  Wire.endTransmission();
-  
-  Wire.requestFrom((uint8_t) DS1307_ADDRESS, size);
-  for (uint8_t pos = 0; pos < size; ++pos) {
-    buf[pos] = Wire._I2C_READ();
-  }
-}
-
-void RTC_DS1307::writenvram(uint8_t address, uint8_t* buf, uint8_t size) {
-  int addrByte = DS1307_NVRAM + address;
-  Wire.beginTransmission(DS1307_ADDRESS);
-  Wire._I2C_WRITE(addrByte);
-  for (uint8_t pos = 0; pos < size; ++pos) {
-    Wire._I2C_WRITE(buf[pos]);
-  }
-  Wire.endTransmission();
-}
-
-uint8_t RTC_DS1307::readnvram(uint8_t address) {
-  uint8_t data;
-  readnvram(&data, 1, address);
-  return data;
-}
-
-void RTC_DS1307::writenvram(uint8_t address, uint8_t data) {
-  writenvram(address, &data, 1);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// RTC_Millis implementation
-
-long RTC_Millis::offset = 0;
-
-void RTC_Millis::adjust(const DateTime& dt) {
-    offset = dt.unixtime() - millis() / 1000;
-}
-
-DateTime RTC_Millis::now() {
-  return (uint32_t)(offset + millis() / 1000);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////////////////////////////////
-// RTC_PCF8563 implementation
-
-boolean RTC_PCF8523::begin(void) {
-  Wire.begin();
-  return true;
-}
-
-boolean RTC_PCF8523::initialized(void) {
-  Wire.beginTransmission(PCF8523_ADDRESS);
-  Wire._I2C_WRITE((byte)PCF8523_CONTROL_3);
-  Wire.endTransmission();
-
-  Wire.requestFrom(PCF8523_ADDRESS, 1);
-  uint8_t ss = Wire._I2C_READ();
-  return ((ss & 0xE0) != 0xE0);
-}
-
-void RTC_PCF8523::adjust(const DateTime& dt) {
-  Wire.beginTransmission(PCF8523_ADDRESS);
-  Wire._I2C_WRITE((byte)3); // start at location 3
-  Wire._I2C_WRITE(bin2bcd(dt.second()));
-  Wire._I2C_WRITE(bin2bcd(dt.minute()));
-  Wire._I2C_WRITE(bin2bcd(dt.hour()));
-  Wire._I2C_WRITE(bin2bcd(dt.day()));
-  Wire._I2C_WRITE(bin2bcd(0)); // skip weekdays
-  Wire._I2C_WRITE(bin2bcd(dt.month()));
-  Wire._I2C_WRITE(bin2bcd(dt.year() - 2000));
-  Wire.endTransmission();
-
-  // set to battery switchover mode
-  Wire.beginTransmission(PCF8523_ADDRESS);
-  Wire._I2C_WRITE((byte)PCF8523_CONTROL_3);
-  Wire._I2C_WRITE((byte)0x00);
-  Wire.endTransmission();
-}
-
-DateTime RTC_PCF8523::now() {
-  Wire.beginTransmission(PCF8523_ADDRESS);
-  Wire._I2C_WRITE((byte)3);	
-  Wire.endTransmission();
-
-  Wire.requestFrom(PCF8523_ADDRESS, 7);
-  uint8_t ss = bcd2bin(Wire._I2C_READ() & 0x7F);
-  uint8_t mm = bcd2bin(Wire._I2C_READ());
-  uint8_t hh = bcd2bin(Wire._I2C_READ());
-  uint8_t d = bcd2bin(Wire._I2C_READ());
-  Wire._I2C_READ();  // skip 'weekdays'
-  uint8_t m = bcd2bin(Wire._I2C_READ());
-  uint16_t y = bcd2bin(Wire._I2C_READ()) + 2000;
-  
-  return DateTime (y, m, d, hh, mm, ss);
-}
-
-Pcf8523SqwPinMode RTC_PCF8523::readSqwPinMode() {
-  int mode;
-
-  Wire.beginTransmission(PCF8523_ADDRESS);
-  Wire._I2C_WRITE(PCF8523_CLKOUTCONTROL);
-  Wire.endTransmission();
-  
-  Wire.requestFrom((uint8_t)PCF8523_ADDRESS, (uint8_t)1);
-  mode = Wire._I2C_READ();
-
-  mode >>= 3;
-  mode &= 0x7;
-  return static_cast<Pcf8523SqwPinMode>(mode);
-}
-
-void RTC_PCF8523::writeSqwPinMode(Pcf8523SqwPinMode mode) {
-  Wire.beginTransmission(PCF8523_ADDRESS);
-  Wire._I2C_WRITE(PCF8523_CLKOUTCONTROL);
-  Wire._I2C_WRITE(mode << 3);
-  Wire.endTransmission();
-}
-
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-// RTC_DS3231 implementation
-
 boolean RTC_DS3231::begin(void) {
-  Wire.begin();
+  //Wire.begin();
   return true;
 }
 
@@ -440,17 +244,35 @@ bool RTC_DS3231::lostPower(void) {
   return (read_i2c_register(DS3231_ADDRESS, DS3231_STATUSREG) >> 7);
 }
 
+void RTC_DS3231::setSpeed(uint16_t speed) {
+    I2C_SCL_FREQUENCY = speed;
+}
+
 void RTC_DS3231::adjust(const DateTime& dt) {
-  Wire.beginTransmission(DS3231_ADDRESS);
-  Wire._I2C_WRITE((byte)0); // start at location 0
-  Wire._I2C_WRITE(bin2bcd(dt.second()));
-  Wire._I2C_WRITE(bin2bcd(dt.minute()));
-  Wire._I2C_WRITE(bin2bcd(dt.hour()));
-  Wire._I2C_WRITE(bin2bcd(0));
-  Wire._I2C_WRITE(bin2bcd(dt.day()));
-  Wire._I2C_WRITE(bin2bcd(dt.month()));
-  Wire._I2C_WRITE(bin2bcd(dt.year() - 2000));
-  Wire.endTransmission();
+  //Wire.beginTransmission(DS3231_ADDRESS);
+  brzo_i2c_start_transaction(DS3231_ADDRESS, I2C_SCL_FREQUENCY);
+  buffer[0] = 0;
+  buffer[1] = bin2bcd(dt.second());
+  buffer[2] = bin2bcd(dt.minute());
+  buffer[3] = bin2bcd(dt.hour());
+
+  //Wire._I2C_WRITE((byte)0); // start at location 0
+  //Wire._I2C_WRITE(bin2bcd(dt.second()));
+  //Wire._I2C_WRITE(bin2bcd(dt.minute()));
+  //Wire._I2C_WRITE(bin2bcd(dt.hour()));
+
+  buffer[4] = 0;
+  buffer[5] = bin2bcd(dt.day());
+  buffer[6] = bin2bcd(dt.month());
+  buffer[7] = bin2bcd(dt.year() - 2000);
+  brzo_i2c_write(buffer, 8, false);
+
+  //Wire._I2C_WRITE(bin2bcd(0));
+  //Wire._I2C_WRITE(bin2bcd(dt.day()));
+  //Wire._I2C_WRITE(bin2bcd(dt.month()));
+  //Wire._I2C_WRITE(bin2bcd(dt.year() - 2000));
+  //Wire.endTransmission();
+  brzo_i2c_end_transaction();
 
   uint8_t statreg = read_i2c_register(DS3231_ADDRESS, DS3231_STATUSREG);
   statreg &= ~0x80; // flip OSF bit
@@ -458,32 +280,61 @@ void RTC_DS3231::adjust(const DateTime& dt) {
 }
 
 DateTime RTC_DS3231::now() {
-  Wire.beginTransmission(DS3231_ADDRESS);
-  Wire._I2C_WRITE((byte)0);	
-  Wire.endTransmission();
+  //Wire.beginTransmission(DS3231_ADDRESS);
+  uint8_t	bcode = 0;
 
-  Wire.requestFrom(DS3231_ADDRESS, 7);
-  uint8_t ss = bcd2bin(Wire._I2C_READ() & 0x7F);
-  uint8_t mm = bcd2bin(Wire._I2C_READ());
-  uint8_t hh = bcd2bin(Wire._I2C_READ());
-  Wire._I2C_READ();
-  uint8_t d = bcd2bin(Wire._I2C_READ());
-  uint8_t m = bcd2bin(Wire._I2C_READ());
-  uint16_t y = bcd2bin(Wire._I2C_READ()) + 2000;
-  
+  brzo_i2c_start_transaction(DS3231_ADDRESS, I2C_SCL_FREQUENCY);
+  buffer[0] = 0;
+  brzo_i2c_write(buffer, 1, false);
+  bcode = brzo_i2c_end_transaction();
+  //Wire._I2C_WRITE((byte)0);
+  //Wire.endTransmission();
+  delay(50);
+  //Wire.requestFrom(DS3231_ADDRESS, 7);
+  if (bcode == 0) {
+      brzo_i2c_start_transaction(DS3231_ADDRESS, I2C_SCL_FREQUENCY);
+      brzo_i2c_read(buffer, 7, false);
+      bcode = brzo_i2c_end_transaction();
+  }
+
+  //uint8_t ss = bcd2bin(Wire._I2C_READ() & 0x7F);
+  uint8_t ss = bcd2bin(buffer[0] & 0x7F);
+  //uint8_t mm = bcd2bin(Wire._I2C_READ());
+  uint8_t mm = bcd2bin(buffer[1]);
+  //uint8_t hh = bcd2bin(Wire._I2C_READ());
+  uint8_t hh = bcd2bin(buffer[2]);
+  //Wire._I2C_READ();
+  //uint8_t d = bcd2bin(Wire._I2C_READ());
+  uint8_t d = bcd2bin(buffer[4]);
+  //uint8_t m = bcd2bin(Wire._I2C_READ());
+  uint8_t m = bcd2bin(buffer[5]);
+  //uint16_t y = bcd2bin(Wire._I2C_READ()) + 2000;
+  uint16_t y = bcd2bin(buffer[6]) + 2000;
+
   return DateTime (y, m, d, hh, mm, ss);
 }
 
 Ds3231SqwPinMode RTC_DS3231::readSqwPinMode() {
+  int bcode = 0;
   int mode;
 
-  Wire.beginTransmission(DS3231_ADDRESS);
-  Wire._I2C_WRITE(DS3231_CONTROL);
-  Wire.endTransmission();
-  
-  Wire.requestFrom((uint8_t)DS3231_ADDRESS, (uint8_t)1);
-  mode = Wire._I2C_READ();
+  brzo_i2c_start_transaction(DS3231_ADDRESS, I2C_SCL_FREQUENCY);
+  buffer[0] = DS3231_CONTROL;
+  brzo_i2c_write(buffer, 1, false);
+  bcode = brzo_i2c_end_transaction();
 
+  //Wire.beginTransmission(DS3231_ADDRESS);
+  //Wire._I2C_WRITE(DS3231_CONTROL);
+  //Wire.endTransmission();
+
+  if (bcode == 0) {
+      brzo_i2c_start_transaction(DS3231_ADDRESS, I2C_SCL_FREQUENCY);
+      brzo_i2c_read(buffer, 1, false);
+      brzo_i2c_end_transaction();
+  }
+  //Wire.requestFrom((uint8_t)DS3231_ADDRESS, (uint8_t)1);
+  //mode = Wire._I2C_READ();
+  mode = buffer[0];
   mode &= 0x93;
   return static_cast<Ds3231SqwPinMode>(mode);
 }
@@ -499,7 +350,7 @@ void RTC_DS3231::writeSqwPinMode(Ds3231SqwPinMode mode) {
     ctrl |= 0x04; // turn on INTCN
   } else {
     ctrl |= mode;
-  } 
+  }
   write_i2c_register(DS3231_ADDRESS, DS3231_CONTROL, ctrl);
 
   //Serial.println( read_i2c_register(DS3231_ADDRESS, DS3231_CONTROL), HEX);
--- RTClib.h
+++ RTClib.h
@@ -8,14 +8,6 @@
 class TimeSpan;
 
 
-#define PCF8523_ADDRESS       0x68
-#define PCF8523_CLKOUTCONTROL 0x0F
-#define PCF8523_CONTROL_3     0x02
-
-#define DS1307_ADDRESS  0x68
-#define DS1307_CONTROL  0x07
-#define DS1307_NVRAM    0x08
-
 #define DS3231_ADDRESS  0x68
 #define DS3231_CONTROL  0x0E
 #define DS3231_STATUSREG 0x0F
@@ -44,7 +36,7 @@ public:
     uint8_t dayOfTheWeek() const;
 
     // 32-bit times as seconds since 1/1/2000
-    long secondstime() const;   
+    long secondstime() const;
     // 32-bit times as seconds since 1/1/1970
     uint32_t unixtime(void) const;
 
@@ -75,29 +67,13 @@ protected:
     int32_t _seconds;
 };
 
-// RTC based on the DS1307 chip connected via I2C and the Wire library
-enum Ds1307SqwPinMode { OFF = 0x00, ON = 0x80, SquareWave1HZ = 0x10, SquareWave4kHz = 0x11, SquareWave8kHz = 0x12, SquareWave32kHz = 0x13 };
-
-class RTC_DS1307 {
-public:
-    boolean begin(void);
-    static void adjust(const DateTime& dt);
-    uint8_t isrunning(void);
-    static DateTime now();
-    static Ds1307SqwPinMode readSqwPinMode();
-    static void writeSqwPinMode(Ds1307SqwPinMode mode);
-    uint8_t readnvram(uint8_t address);
-    void readnvram(uint8_t* buf, uint8_t size, uint8_t address);
-    void writenvram(uint8_t address, uint8_t data);
-    void writenvram(uint8_t address, uint8_t* buf, uint8_t size);
-};
-
 // RTC based on the DS3231 chip connected via I2C and the Wire library
 enum Ds3231SqwPinMode { DS3231_OFF = 0x01, DS3231_SquareWave1Hz = 0x00, DS3231_SquareWave1kHz = 0x08, DS3231_SquareWave4kHz = 0x10, DS3231_SquareWave8kHz = 0x18 };
 
 class RTC_DS3231 {
 public:
     boolean begin(void);
+    void setSpeed(uint16_t speed);
     static void adjust(const DateTime& dt);
     bool lostPower(void);
     static DateTime now();
@@ -105,20 +81,6 @@ public:
     static void writeSqwPinMode(Ds3231SqwPinMode mode);
 };
 
-
-// RTC based on the PCF8523 chip connected via I2C and the Wire library
-enum Pcf8523SqwPinMode { PCF8523_OFF = 7, PCF8523_SquareWave1HZ = 6, PCF8523_SquareWave32HZ = 5, PCF8523_SquareWave1kHz = 4, PCF8523_SquareWave4kHz = 3, PCF8523_SquareWave8kHz = 2, PCF8523_SquareWave16kHz = 1, PCF8523_SquareWave32kHz = 0 };
-
-class RTC_PCF8523 {
-public:
-    boolean begin(void);
-    void adjust(const DateTime& dt);
-    boolean initialized(void);
-    static DateTime now();
-
-    Pcf8523SqwPinMode readSqwPinMode();
-    void writeSqwPinMode(Pcf8523SqwPinMode mode);
-};
 
 // RTC using the internal millis() clock, has to be initialized before use
 // NOTE: this clock won't be correct once the millis() timer rolls over (>49d?)
